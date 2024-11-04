0703f88b7f0b934ae82063be05a3f2d888e0e491
LaviniaBonSab <118178956+LaviniaBonSab@users.noreply.github.com>
2024-11-04 14:32:40 +0100

    aggiornamento_files

--- js/script.js
+++ js/script.js
@@ -31,3 +31,35 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+
+
+
+
+
+
+// PAGINA DEL CATALOGO
+// Accordion
+
+<script>
+  // Select all elements with the class "accordion"
+  const accordions = document.querySelectorAll(".accordion");
+
+  // Loop through each accordion button and add a click event listener
+  accordions.forEach((accordion) => {
+    accordion.addEventListener("click", function() {
+      // Toggle the "active" class to highlight the accordion button
+      this.classList.toggle("active");
+
+      // Get the next sibling element, which is the panel to show/hide
+      const panel = this.nextElementSibling;
+      
+      // Toggle the panel's display property
+      if (panel.style.display === "block") {
+        panel.style.display = "none";
+      } else {
+        panel.style.display = "block";
+      }
+    });
+  });
+</script>
